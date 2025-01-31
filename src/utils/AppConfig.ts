a3547a7623d1fb1af6203e04dd0398a6170e04bb
TETRAnoctua <tetranoctua@gmail.com>
2025-01-30 19:36:55 -0800

    ss

--- src/utils/AppConfig.ts
+++
@@ -1,8 +0,0 @@
-export const AppConfig = {
-  site_name: 'TETRAnoctua',
-  title: 'TETRAnoctua',
-  description: 'Generating Noise!',
-  author: 'TETRA',
-  locale_region: 'en-us',
-  locale: 'en',
-};
